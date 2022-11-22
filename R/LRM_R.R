13f5d624af3cec69106eb8f09c6f3815562142cd
Wenjing Li <wjingli@umich.edu>
2022-11-21 23:03:56 -0500

    change in LRM

--- R/LRM_R.R
+++ R/LRM_R.R
@@ -1,8 +1,38 @@
+#'Linear Regression Model using R
+#'
+#'LRM_R is used to implementing simple and multiple linear regression using least squares method in R.
+#'It can carry out the estimated value，standard deviation and statistics value of coefficients
+#'
+#'
+#' @param formula  a symbolic description of the model to be fitted.
+#' @param x=FALSE  logicals.It is TRUE after the model matrix returned.
+#' @param y=FALSE  logicals.It is TRUE after the reponse matrix returned.
+#'
+#'
+#'@returns LRM_R returns a set of simple and multiple linear regression result of formula.
+#'An object of class "LRM_R" is a list containing at least the following components:
+#'\itemize{
+#'   \item coefficients -  The estimated value, standard deviation and statistics value of coefficients
+#'   \item r.squared - The coefficient of determination of the fitted model
+#'   \item fstatistic - The result of F-statistic of the fitted model
+#'   \item Y_model - The result of linear regression for response matrix
+#' }
+#'
+#'@examples
+#'#Using R's default dataset
+#'LRM_R(mtcars$mpg~mtcars$disp +mtcars$wt+mtcars$qsec )
+#'
+#'#In addition to data frames, it also supply vectors.
+#'a <- c(1.1,2.5,4.1,5.0,6.5,2.6,1.1,3.5,3.3,4.1)
+#'b <- c(4.8,4.1,4.4,3.5,5.8,3.8,6.0,4.8,4.3,4.6)
+#'LRM_R(a~b)$coefficients
+#'LRM_R(a~b)$r.squared
+#'
+#'
+#'@export
+#'
 
-
-
-LRM_R <- function(formula, df, model = TRUE, x = FALSE, y = FALSE){
-
+LRM_R <- function(formula,x = FALSE, y = FALSE){
 #Find model frame, response variables and covariance
   MF1 = model.frame(formula)
   Y = model.response(MF1)
@@ -37,13 +67,9 @@ LRM_R <- function(formula, df, model = TRUE, x = FALSE, y = FALSE){
   coefficients =cbind("Estimate"=c(coeff), "Std. Error" =sd_coeff,"t value" =t_stat,"Pr(>|t|)" =p_value_t)
 
 # build result
-  result <- list(coefficients = coefficients, r.squared = R_2 ,fstatistic = F_stat)
+  result <- list(coefficients = coefficients, r.squared = R_2 ,fstatistic = F_stat,Y_model = Y_e)
 
 return(result)
 
 }
 
-bench::mark(summary(lm(H$weight~H$age +H$height ))$coef, LRM_R(H$weight~H$age +H$height ))
-all.equal(summary(lm(H$weight~H$age +H$height ))$coef,LRM_R(H$weight~H$age +H$height)
-aa$r.squared
-as.numeric(aa$fstatistic[1])
